0e1be75eb5661fda883a0d4b5d386a22444be453
Ira Horecka <ira89@icloud.com>
2020-05-07 17:53:25 -0700

    get media static image loaded to bikes.html

--- personal_website/frontpage/models.py
+++ personal_website/frontpage/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     bike_make = models.CharField(max_length=100)
     bike_make_url = models.CharField(max_length=256)
     bike_model = models.TextField()
+    bike_img = models.ImageField(upload_to='bike_imgs/')  # currently not working...
     content = models.TextField()  
     date_posted = models.DateTimeField(default=timezone.now)  # don't call func yet
     author = models.ForeignKey(User, on_delete=models.CASCADE)
